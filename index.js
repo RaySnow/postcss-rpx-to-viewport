dd480ed2c432d7466924b1afb5de7aabf3079112
hupo <r.li@dface.cn>
2018-12-05 17:19:03 +0800

    debut

--- index.js
+++ index.js
@@ -3,53 +3,19 @@
 var postcss = require('postcss');
 var objectAssign = require('object-assign');
 
-var defaults = {
-  unitToConvert: 'rpx', // need convert
-  viewportWidth: 750,   // design draft width
+var defaultsOpt = {
+  targetUnit: 'rpx', // need convert
+  outputUnit: 'vw',   // convert to
+  layoutWidth: 750,   // design draft width
   unitPrecision: 5,     // the decimal numbers
-  viewportUnit: 'vw',   // convert to
-  minPixelValue: 1,     // min value that will not be convert
   mediaQuery: false     // todo 待调研
 };
 
-module.exports = postcss.plugin('postcss-rpx-to-viewport', function (options) {
-
-  var opts = objectAssign({}, defaults, options);
-  var pxReplace = createPxReplace(opts.viewportWidth, opts.minPixelValue, opts.unitPrecision, opts.viewportUnit);
-
-  // excluding regex trick: http://www.rexegg.com/regex-best-trick.html
-  // Not anything inside double quotes
-  // Not anything inside single quotes
-  // Not anything inside url()
-  // Any digit followed by px
-  // !singlequotes|!doublequotes|!url()|pixelunit
-  var pxRegex = new RegExp('"[^"]+"|\'[^\']+\'|url\\([^\\)]+\\)|(\\d*\\.?\\d+)' + opts.unitToConvert, 'ig')
-
-  return function (css) {
-
-    css.walkDecls(function (decl, i) {
-      // This should be the fastest test and will remove most declarations
-      if (decl.value.indexOf(opts.unitToConvert) === -1) return;
-
-      decl.value = decl.value.replace(pxRegex, createPxReplace(opts.viewportWidth, opts.minPixelValue, opts.unitPrecision, viewportUnit));
-    });
-
-    if (opts.mediaQuery) {
-      css.walkAtRules('media', function (rule) {
-        if (rule.params.indexOf(opts.unitToConvert) === -1) return;
-        rule.params = rule.params.replace(pxRegex, pxReplace);
-      });
-    }
-
-  };
-});
-
-function createPxReplace(viewportSize, minPixelValue, unitPrecision, viewportUnit) {
+function generateReplacement(layoutWidth, unitPrecision, outputUnit) {
   return function (m, $1) {
     if (!$1) return m;
     var pixels = parseFloat($1);
-    if (pixels <= minPixelValue) return m;
-    return toFixed((pixels / (viewportSize / 2) * 100), unitPrecision) + viewportUnit;
+    return toFixed((pixels / (layoutWidth / 2) * 100), unitPrecision) + outputUnit;
   };
 }
 
@@ -59,3 +25,23 @@ function toFixed(number, precision) {
   return Math.round(wholeNumber / 10) * 10 / multiplier;
 }
 
+
+module.exports = postcss.plugin('postcss-rpx-to-viewport', function (options) {
+  var opts = objectAssign({}, defaultsOpt, options);
+  var pxRegex = new RegExp('"[^"]+"|\'[^\']+\'|url\\([^\\)]+\\)|(\\d*\\.?\\d+)' + opts.targetUnit, 'ig');
+
+  return function (root) {
+    root.walkRules(function (rule) {
+      rule.walkDecls(function (decl) {
+        if (decl.value.indexOf(opts.targetUnit) === -1) return;
+        decl.value = decl.value.replace(pxRegex, generateReplacement(opts.layoutWidth, opts.unitPrecision, outputUnit));
+      })
+    });
+    if (opts.mediaQuery) {
+      root.walkAtRules('media', function (rule) {
+        if (rule.params.indexOf(opts.targetUnit) === -1) return;
+        rule.params = rule.params.replace(pxRegex, generateReplacement(opts.layoutWidth, opts.unitPrecision, opts.outputUnit));
+      });
+    }
+  };
+});
